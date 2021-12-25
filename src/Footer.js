bf1b92f79a984263b777055d4a2e3b76873d1b7f
Mark-James McDougall <markjames@hh-dev.com>
2021-12-24 20:39:31 -0500

    - Footer update

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer footer-text">
-    <p>Created by <a href="https://markjames.dev" text-color="white">Mark-James M</a>.</p>
+    <p>Created by <a href="https://markjames.dev" text-color="white">Mark-James M</a> • <a href="https://github.com/markjamesm/zoltan">Github</a></p>
   </div>
 );
 
